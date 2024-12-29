59abdddc43c28ef100b5c4da433f9b41a64e4d9f
Dnilson Sousa <dnilsonsousaoficial17@gmail.com>
2024-12-29 14:31:18 -0300

    updated to dual formulation of the problem

--- src/svm.py
+++ src/svm.py
@@ -1,17 +1,21 @@
 import numpy as np
 from qpsolvers import solve_qp
+from scipy.sparse import csc_matrix
 
 class SVM:
-    '''
-    Classe para implementação de um classificador de Máquina de Vetores de Suporte (SVM - Support Vector Machine)
-    utilizando Programação Quadrática para otimização.
+    """
+    Implementação de uma Máquina de Vetores de Suporte (SVM - Support Vector Machine)
+    utilizando Programação Quadrática para encontrar o hiperplano ótimo que separa duas classes.
 
     Atributos:
     ----------
     weights : np.ndarray
-        Vetor de pesos (coeficientes) da hiperplano após o treinamento.
+        Vetor de pesos (coeficientes) do hiperplano após o treinamento.
     bias : float
         Bias (termo independente) do hiperplano após o treinamento.
+    C : float
+        Parâmetro de regularização que controla o trade-off entre maximizar a margem e minimizar
+        violações na classificação.
 
     Métodos:
     --------
@@ -34,18 +38,23 @@ class SVM:
         Matriz de dados de teste com dimensão (M x P), onde:
         - M é o número de amostras de teste.
         - P é o número de atributos (features) por amostra.
-    '''
+    """
 
-    def __init__(self):
-        '''
+    def __init__(self, C: float = 1.0):
+        """
         Inicializa a classe SVM.
-        Define os pesos e o bias como None, pois serão definidos após o treinamento (fit).
-        '''
+
+        Parâmetros:
+        -----------
+        C : float, opcional (default=1.0)
+            Parâmetro de regularização que controla o trade-off entre margem e violações.
+        """
         self.weights = None
         self.bias = None
+        self.C = C
 
     def fit(self, x_train, y_train):
-        '''
+        """
         Treina o modelo SVM utilizando Programação Quadrática para encontrar o hiperplano ótimo.
 
         Parâmetros:
@@ -53,37 +62,45 @@ class SVM:
         x_train : np.ndarray
             Matriz de treinamento (N x P), onde:
             - N é o número de amostras.
-            - P é o número de atributos por amostra.
+            - P é o número de atributos (features) por amostra.
         y_train : np.ndarray
-            Vetor de rótulos (N,) das amostras de treinamento.
+            Vetor de rótulos das amostras de treinamento (N,).
             - Deve conter apenas valores -1 e 1.
 
-        O algoritmo resolve o problema de otimização quadrática da seguinte forma:
+        Detalhes:
+        ---------
+        Resolve o problema de otimização quadrática da forma:
             minimize    (1/2) * x.T @ P @ x + q.T @ x
             subject to  G @ x <= h
+                        A @ x = b
 
-        Após resolver, os pesos (`weights`) e o bias (`bias`) são extraídos do vetor solução.
-        '''
-        # Matriz P define os pesos e desativa penalização do bias
-        P = np.eye(x_train.shape[1] + 1); P[-1, -1] = 0  # Termo relacionado ao bias não deve ser penalizado
+        Após resolver, os pesos (`weights`) e o bias (`bias`) são extraídos da solução.
+        """
+        # Construção do problema dual
+        B = x_train @ x_train.T  # Produto interno das amostras
+        S = np.outer(y_train, y_train)  # Produto externo para aplicar restrições
+        P = csc_matrix(S * B)  # Matriz de penalização
+        q = -1 * np.ones(len(y_train))  # Vetor do problema dual
 
-        # Vetor q (componente linear do problema de otimização)
-        q = np.zeros(shape=(x_train.shape[1] + 1, 1))
+        # Restrições de igualdade
+        A = csc_matrix(y_train[np.newaxis, :])  # Vetor para garantir a soma dos alphas
+        b = np.zeros((1,))
 
-        # Restrições do problema: G @ x <= h
-        G = x_train * y_train[:, np.newaxis] 
-        G = -1 * np.concatenate([G, y_train[:, np.newaxis]], axis=1) 
-        h = -1 * np.ones(shape=(x_train.shape[0], 1))
+        # Restrições de desigualdade (limites inferiores e superiores)
+        lb = np.zeros(len(y_train))  # alpha >= 0
+        lu = self.C * np.ones(len(y_train))  # alpha <= C
 
-        # Resolver o problema de programação quadrática
-        x = solve_qp(P, q, G, h, solver="osqp")
+        # Solução do problema de Programação Quadrática
+        alpha = solve_qp(P, q, None, None, A, b, lb, lu, solver="clarabel")
 
-        # Extração dos pesos e do bias da solução
-        self.weights = x[:-1]
-        self.bias = x[-1]
+        # Cálculo dos pesos do modelo
+        self.weights = np.sum((alpha * y_train)[:, np.newaxis] * x_train, axis=0)
 
-    def predict(self, x_test):
-        '''
+        # Cálculo do bias usando todos os vetores de suporte
+        self.bias = np.mean(y_train - np.sum(self.weights[np.newaxis, :] * x_train, axis=1))
+
+    def predict(self, x_test: np.ndarray) -> np.ndarray:
+        """
         Prediz as classes das amostras de teste com base no modelo treinado.
 
         Parâmetros:
@@ -98,6 +115,7 @@ class SVM:
         y_predict : np.ndarray
             Vetor de predições (M,), contendo os rótulos preditos para as amostras:
             -1 ou 1.
-        '''
-        y_predict = np.sign(x_test @ self.weights[:, np.newaxis] + self.bias)
+        """
+        # Calcula a predição como o sinal da função de decisão
+        y_predict = np.sign(x_test @ self.weights + self.bias)
         return y_predict
